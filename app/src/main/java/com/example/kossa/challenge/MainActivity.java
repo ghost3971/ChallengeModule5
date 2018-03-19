4ea959a57e943e4c4bcc6c005fc4ac12f69041ac
KOSSA <kossa.promess@gmail.com>
2018-03-19 10:33:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kossa/challenge/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kossa.challenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.example.kossa.challenge.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
